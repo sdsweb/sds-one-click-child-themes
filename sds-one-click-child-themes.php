72b092af09eb5118d13311f692a3abf4d5be4aa0
scottsousa <scott@scottjsousa.com>
2014-01-31 10:45:37 -0500

    Adjusted Repo Link & Comment

--- sds-one-click-child-themes.php
+++ sds-one-click-child-themes.php
@@ -10,7 +10,7 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       /languages
- * GitHub Plugin URI: https://github.com/sdsweb/One-Click-Child-Themes-WordPress-Plugin/
+ * GitHub Plugin URI: https://github.com/sdsweb/sds-one-click-child-themes/
  *
  * @package   SDS_One_Click_Child_Themes
  * @author    Slocum Studio
